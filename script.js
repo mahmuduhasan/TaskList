b9a14013e8b8a4d2b04f399c819afb2b379a29fe
mahmuduhasan <codermodule@gmail.com>
2020-12-06 11:22:54 +0600

    gh-page update

--- script.js
+++ script.js
@@ -4,8 +4,18 @@ let clearTask = document.querySelector("#clear");
 let filterTask = document.querySelector("#filter-task");
 let newTask = document.querySelector("#new-task");
 
+const localStorageHandler = (task) => {
+  let allTasks;
+  if (localStorage.getItem("allTasks") === null) {
+    allTasks = [];
+  } else {
+    allTasks = JSON.parse(localStorage.getItem("allTasks"));
+  }
+  allTasks.push(task);
+  localStorage.setItem("allTasks", JSON.stringify(allTasks));
+};
+
 const taskSubmitHandler = (event) => {
-  console.log(event);
   if (newTask.value == "") {
     alert("Enter Task!");
   } else {
@@ -16,6 +26,7 @@ const taskSubmitHandler = (event) => {
     deleteTask.innerHTML = "x";
     li.appendChild(deleteTask);
     taskList.appendChild(li);
+    localStorageHandler(newTask.value);
     newTask.value = "";
   }
   event.preventDefault();
@@ -26,6 +37,7 @@ const removeTaskHandler = (event) => {
     if (confirm("Are you sure?")) {
       let selectedElement = event.target.parentElement;
       selectedElement.remove();
+      removeFromLocalStorage(selectedElement);
     }
   }
 };
@@ -40,8 +52,59 @@ const clearTaskHandler = (event) => {
       }
     }
   }
+  localStorage.clear();
 };
 
+const filterTaskHandler = (event) => {
+  let searchText = filterTask.value.toLowerCase();
+  document.querySelectorAll("li").forEach((taskName) => {
+    let taskItem = taskName.firstChild.textContent;
+    if (taskItem.toLowerCase().indexOf(searchText) != -1) {
+      taskName.style.display = "block";
+    } else {
+      taskName.style.display = "none";
+    }
+  });
+};
+
+const loadHandler = () => {
+  let allTasks;
+  if (localStorage.getItem("allTasks") === null) {
+    allTasks = [];
+  } else {
+    allTasks = JSON.parse(localStorage.getItem("allTasks"));
+  }
+  allTasks.forEach((task) => {
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode(task + " "));
+    let deleteTask = document.createElement("a");
+    deleteTask.setAttribute("href", "#");
+    deleteTask.innerHTML = "x";
+    li.appendChild(deleteTask);
+    taskList.appendChild(li);
+  });
+};
+
+const removeFromLocalStorage = (singleItem) => {
+  let allTasks;
+  if (localStorage.getItem("allTasks") === null) {
+    allTasks = [];
+  } else {
+    allTasks = JSON.parse(localStorage.getItem("allTasks"));
+  }
+
+  let li = singleItem;
+  li.removeChild(li.lastChild);
+
+  allTasks.forEach((task, index) => {
+    if (li.textContent.trim() === task) {
+      allTasks.splice(index, 1);
+    }
+  });
+  localStorage.setItem("allTasks", JSON.stringify(allTasks));
+};
 taskForm.addEventListener("submit", taskSubmitHandler);
 taskList.addEventListener("click", removeTaskHandler);
 clearTask.addEventListener("click", clearTaskHandler);
+filterTask.addEventListener("keyup", filterTaskHandler);
+document.addEventListener("DOMContentLoaded", loadHandler);
